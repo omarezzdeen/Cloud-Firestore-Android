d8468404f77db60819fb6b8bfc892c4f9848b1ea
omar ezzdeen <omar.izziddeen@gmail.com>
2022-03-04 16:48:45 +0200

    add binding

--- app/src/main/java/com/example/cloudfirestoreandroid/MainActivity.kt
+++ app/src/main/java/com/example/cloudfirestoreandroid/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.cloudfirestoreandroid
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Firebase.firestore
+
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "omar",
+            "last" to "ezzdeen",
+            "born" to 1815
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users").document("user1")
+            .set(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: $documentReference")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
     }
 }
